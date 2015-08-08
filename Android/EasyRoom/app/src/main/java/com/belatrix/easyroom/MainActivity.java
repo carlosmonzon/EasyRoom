4dcd7fb0c6ae8367bc26eae8176fe4222d0b07bf
gyosida <gianfranco.yosida@hotmail.com>
2015-08-07 20:51:11 -0500

    retrofit

--- Android/EasyRoom/app/src/main/java/com/belatrix/easyroom/MainActivity.java
+++ Android/EasyRoom/app/src/main/java/com/belatrix/easyroom/MainActivity.java
@@ -19,6 +19,10 @@ import com.belatrix.easyroom.webservices.NetworkingHelper;
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
 public class MainActivity extends AppCompatActivity {
 
     private RoomsAdapter mRoomsAdapter;
@@ -36,7 +40,17 @@ public class MainActivity extends AppCompatActivity {
 
         initUI();
 //        NetworkingHelper.getInstance().getRoomService().getRooms();
-        NetworkingHelper.getInstance().getRoomService().getRooms()
+//        NetworkingHelper.getInstance().getRoomService().getRooms(new Callback<String>() {
+//            @Override
+//            public void success(String string, Response response) {
+//
+//            }
+//
+//            @Override
+//            public void failure(RetrofitError error) {
+//                RetrofitError e = error;
+//            }
+//        });
 
     }
 
