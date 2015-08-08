8e42d8497d7bc988afc0b35841fb9e21bb287773
carlosmonzon <carlosmonzonsalvador@gmail.com>
2015-08-07 19:01:03 -0500

    android styles

---
+++ Android/EasyRoom/app/src/main/java/com/belatrix/easyroom/ui/fragments/DayListAdapter.java
@@ -0,0 +1,25 @@
+package com.belatrix.easyroom.ui.fragments;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by cmonzon on 07/08/15.
+ */
+public class DayListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
